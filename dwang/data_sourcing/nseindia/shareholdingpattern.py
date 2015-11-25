648c29d962edecb319e0ebdfbe49cf0b333a7c99
dingwangnipuncapital <ding.wang@nipuncapital.com>
2015-11-25 11:30:28 -0800

    for india nse, try to download first then parse

--- dwang/data_sourcing/nseindia/shareholdingpattern.py
+++ dwang/data_sourcing/nseindia/shareholdingpattern.py
@@ -39,29 +39,59 @@ countString = "countString"
 Pattern = "Pattern"
 
 
-def all_comp_names(save2f=True):
-    all_list = requests.post(base_url+jsp_high, data={'start':0, 'limit':2000}, timeout=30).content.strip()
-    records = eval(all_list)
-    if records[success] != 'true': raise Exception('all_list failed.')
-    all_df = pd.DataFrame(records[rows])
+def all_comp_names(save2f=True, rff=True):#rff read file first
 
-    if save2f:
-        all_df.to_csv('./data/all_comp_names.csv', index=None)
+    fn = './data/all_comp_names.csv'
+    dodld = False # do download
+    
+    if rff:
+        if os.path.exists(fn):
+            all_df = pd.read_csv(fn)
+        else:
+            dodld = True
+            
+    if (not rff) or dodld:
+        all_list = requests.post(base_url+jsp_high, data={'start':0, 'limit':2000}, timeout=30).content.strip()
+        records = eval(all_list)
+        if records[success] != 'true': raise Exception('all_list failed.')
+        all_df = pd.DataFrame(records[rows])
+
+        if save2f:
+            all_df.to_csv(fn, index=None)
+        
     return all_df
 
 
-def all_dates(symb, save2f=True):#for one company, find all qtr ends
-    all_list_dates_df = pd.DataFrame()
-    all_dates = requests.post(base_url+jsp_high, data={'start':0, 'limit':2000, 'symbol':symb}, timeout=30).content.strip()
-    all_dates_records = eval(all_dates)
-    all_dates_df = pd.DataFrame(all_dates_records[rows])
-    all_list_dates_df = all_list_dates_df.append(all_dates_df, ignore_index=True)
+def all_dates(symb, save2f=True, rff=True):#for one company, find all qtr ends
 
-    if save2f:
-        all_list_dates_df.to_csv('./data/'+symb+'_dates.csv', index=None)
-    print "all dates for %s"%symb
-    return all_list_dates_df
+    errf = open('./data/errors.err', 'a')
+    fn = './data/'+symb+'_dates.csv'
+    dodld = False # do download
+    
+    try:
 
+        if rff:
+            if os.path.exists(fn):
+                all_list_dates_df = pd.read_csv(fn)
+            else:
+                dodld = True
+
+        if (not rff) or dodld:
+            all_list_dates_df = pd.DataFrame()
+            all_dates = requests.post(base_url+jsp_high, data={'start':0, 'limit':2000, 'symbol':symb}, timeout=30).content.strip()
+            all_dates_records = eval(all_dates)
+            all_dates_df = pd.DataFrame(all_dates_records[rows])
+            all_list_dates_df = all_list_dates_df.append(all_dates_df, ignore_index=True)
+
+            if save2f:
+                all_list_dates_df.to_csv(fn, index=None)
+            print "all dates for %s"%symb
+        return all_list_dates_df
+
+    except Exception, e:
+        errf.write(fn+'\n')
+    finally:
+        errf.close()
 
 
 def get_all_on_a_date(dt_str, save2f=True):#dt_str like '30-SEP-2015'
@@ -72,7 +102,6 @@ def get_all_on_a_date(dt_str, save2f=True):#dt_str like '30-SEP-2015'
     if save2f:
         all_on_this_date_df.to_csv('./data/'+'all_comp_on_%s.csv'%dt_str, index=None)
     return all_on_this_date_df
-
 
 
 def parse_new(r):
@@ -100,7 +129,7 @@ def parse_new(r):
             col6 = s2f(tds[6].text.strip().replace(',', ''))
             col7 = s2f(tds[7].text.strip().replace(',', ''))
             col8 = s2f(tds[8].text.strip().replace(',', ''))
-        else:
+        elif len(tds)==2:
             tmp = tds[0].text.strip()
             if tmp == u'(A)':
                 cat_shhder1 = 'A'
@@ -113,8 +142,10 @@ def parse_new(r):
             elif tmp == u'(2)':
                 cat_shhder2 = 2
             continue
+        else:
+            continue
 
-        col9 = [i for i in s.findAll('tr') if 'as on' in i.text.lower()][-1].findAll('td')[-1].text
+        col9 = [i for i in s.findAll('tr') if 'As on' in i.text][-1].findAll('td')[-1].text
         col10 = [i for i in s.findAll('tr') if 'nse symbol' in i.text.lower()][-1].findAll('td')[-1].text
         res_list.append([col0, col1, col2, col3, col4, col5, col6, col7, col8, col9, col10])
 
@@ -146,7 +177,7 @@ def parse_old(r):
             col6 = s2f(tds[6].text.strip().replace(',', ''))
             col7 = None
             col8 = None
-        else:
+        elif len(tds)==2:
             tmp = tds[0].text.strip()
             if tmp == u'(A)':
                 cat_shhder1 = 'A'
@@ -159,6 +190,8 @@ def parse_old(r):
             elif tmp == u'(2)':
                 cat_shhder2 = 2
             continue
+        else:
+            continue
 
         if not col1: continue
         col9 = [i for i in s.findAll('tr') if 'Quarter Ended on'.lower() in i.text.lower()][-1].findAll('td')[-1].text
@@ -169,58 +202,72 @@ def parse_old(r):
 
 
 
-def downloader(all_list_dates_df, col_n, w2file = True, db = True, fromfilefirst=False):
-    for i in all_list_dates_df.iterrows():
+def downloader(all_list_dates_df, col_n, w2file = True, db = True, fromfilefirst=False, noparse=False):
 
-        tmp_S = i[1]
-        fn = './data/'+tmp_S['symbol']+'_'+tmp_S['asOnDate']+'.txt'
-        check_web = True
+    errf = open('./data/errors.err', 'a')
+    try:
+        for i in all_list_dates_df.iterrows():
 
-        if fromfilefirst:#read from ./data/files
-            if os.path.exists(fn):
-                with open(fn, 'r') as f:
-                    r = f.read()
-                    check_web = False
-                    if 'nse symbol' not in r.lower():
-                        continue
+            tmp_S = i[1]
+            fn = './data/'+tmp_S['symbol']+'_'+tmp_S['asOnDate']+'.txt'
+            check_web = True
+            ###########################
+            if fromfilefirst:#read from ./data/files
+                if os.path.exists(fn):
+                    with open(fn, 'r') as f:
+                        r = f.read()
+                        check_web = False
+                        if 'nse symbol' not in r.lower():
+                            continue
+            ###########################
+            if (not fromfilefirst) or (check_web):# read from website
+                jsp_str = jsp_low_new if tmp_S['Pattern']=='New' else jsp_low_old
+                r = requests.post(base_url+'ShareholdingPattern/'+jsp_str, data={'ndsId':tmp_S['recordID'], 'symbol':tmp_S['symbol'], 'countStr':tmp_S['countString'], 'asOnDate':tmp_S['asOnDate'],'industry':tmp_S['Ind']}, timeout=30).content
+                print fn
+                print 'wait for 30s'
+                time.sleep(30)
+
+                if 'nse symbol' not in r.lower():
+                    continue
+
+                if w2file:
+                    with open(fn, 'w') as f:
+                        f.write(r)
+            ############################
+            if not noparse:
+                parse = parse_new if tmp_S['Pattern']=='New' else parse_old
+                df = pd.DataFrame(parse(r), columns = col_n)
+                df['last_modification'] = dt.datetime.today().strftime('%Y-%m-%d %H:%M:%S')
+                if db:
+                    sql_io.write_frame(df, 'dwang..india_share', bulk='off', if_exists='append', unic=True)
+
+    except Exception,e:
+        errf.write(tmp_S['symbol']+'_'+tmp_S['asOnDate']+'\n')
+        #errf.write('\n---------------------------------------------------\n')
+    finally:
+        errf.close()
         
-        if (not fromfilefirst) or (check_web):# read from website
-            jsp_str = jsp_low_new if tmp_S['Pattern']=='New' else jsp_low_old
-            r = requests.post(base_url+'ShareholdingPattern/'+jsp_str, data={'ndsId':tmp_S['recordID'], 'symbol':tmp_S['symbol'], 'countStr':tmp_S['countString'], 'asOnDate':tmp_S['asOnDate'],'industry':tmp_S['Ind']}, timeout=30).content
-            time.sleep(20)
-            
-            if 'nse symbol' not in r.lower():
-                continue
-            
-            if w2file:
-                with open(fn, 'w') as f:
-                    f.write(r)
-            
-            
-        parse = parse_new if tmp_S['Pattern']=='New' else parse_old
-        df = pd.DataFrame(parse(r), columns = col_n)
-        df['last_modification'] = dt.datetime.today().strftime('%Y-%m-%d %H:%M:%S')
-        if db:
-            sql_io.write_frame(df, 'dwang..india_share', bulk='off', if_exists='append', unic=True)
-
 if __name__ == '__main__':
 
     myparser = OptionParser()
     myparser.add_option('--fromfilefirst', '-f', dest='fff', action='store_true')
     myparser.add_option('--qtrend', '-q', dest='qtrend', default=None)#should be like '30-SEP-2015'
+    myparser.add_option('--noparse', dest='noparse', action='store_true')
     opts, _ = myparser.parse_args()
     col_n = ['category', 'category of shareholder', 'number of shareholders', 'total number of shares', 'number of shares held in dematerialized form', 'pct of shares (A+B)', 'pct of shares (A+B+C)', 'number of shares', 'pct No. of shares', 'asondate', 'nse symbol']
 
     if opts.qtrend:#if give a specific quarter end date
         all_list_on_a_date_df = get_all_on_a_date(dt_str)
-        downloader(all_list_on_a_date_df, col_n, fromfilefirst=opts.fff)
+        downloader(all_list_on_a_date_df, col_n, fromfilefirst=opts.fff, noparse=opts.noparse)
     
     else:
 
-        all_df = all_comp_names() # names for all companies
+        all_df = all_comp_names(rff=opts.fff) # names for all companies
         for symb in all_df['symbol']:
             print "start %s --------------"%symb
-            all_list_dates_df = all_dates(symb) # all (name, date) combination
-            downloader(all_list_dates_df, col_n, fromfilefirst=opts.fff)
+            all_list_dates_df = all_dates(symb, rff=opts.fff) # all (name, date) combination
+            print 'wait for 20s'
+            time.sleep(20)
+            downloader(all_list_dates_df, col_n, fromfilefirst=opts.fff, noparse=opts.noparse)
             print "end %s --------------"%symb
     
