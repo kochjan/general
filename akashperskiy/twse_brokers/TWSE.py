f9416e0caaf6d6a88fd8c2ed9302be6f6aac0876
Artur.Kashperskiy@nipuncapital.com <Artur.Kashperskiy@nipuncapital.com>
2017-06-16 13:47:29 -0700

    moved stuff around

---
+++ akashperskiy/twse_brokers/TWSE.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+######################################################################
+## Copyright (c) Nipun Capital, L.P., 2011-2017.  All rights reserved.
+##
+## CONFIDENTIAL
+##
+## This unpublished material is proprietary to Nipun Capital, L.P.
+## The algorithms, methods, techniques and information disclosed or
+## described herein are trade secrets and/or confidential information
+## owned by Nipun Capital, L.P.  Any use, reproduction or
+## distribution, in whole or in part, is prohibited, except by express
+## written permission of Nipun Capital, L.P.
+######################################################################
+import requests
+import io
+from PIL import Image
+import captcha
+from bs4 import BeautifulSoup
+import pandas as pd
+import os
+import datetime
+import dateutil
+import progressbar
+
+class TWSE:
+    def __init__(self, stock_ids):
+        self.rs = requests.session()
+        self.base_url = "http://bsr.twse.com.tw/bshtm/"
+        self.stock_ids = stock_ids
+        self.base_page = "bsMenu.aspx"
+        self.csv_page = "bsContent.aspx"
+        self.data = {}
+        self.ocr = captcha.CaptchaRecognize()
+        self.ocr.load_model()
+
+    def get_captcha(self, src):
+        captcha = self.rs.get(self.base_url + src, stream=True, verify=False)
+        return captcha.content
+
+    def get_captcha_src(self):
+        req = self.rs.get(self.base_url + self.base_page)
+        dom = BeautifulSoup(req.text, "html.parser")
+        try:
+            panel = dom.find("div", {"id":"Panel_bshtm"})
+            img_src = panel.find('img')['src']
+        except AttributeError:
+            self.rs = requests.session()
+            return self.get_captcha_src()
+        self.VIEWSTATE = dom.select('#__VIEWSTATE')[0].attrs['value']
+        self.EVENTVALIDATION = dom.select('#__EVENTVALIDATION')[0].attrs['value']
+        return img_src
+
+    def payload_once(self, prediction, stock_id):
+        payload ={
+            '__EVENTTARGET' : '',
+            '__EVENTARGUMENT' : '',
+            '__LASTFOCUS' : '',
+            '__VIEWSTATE' : '%s' % self.VIEWSTATE,
+            '__EVENTVALIDATION' : '%s' % self.EVENTVALIDATION,
+            'RadioButton_Normal' : 'RadioButton_Normal',
+            'TextBox_Stkno' : '%s' % str(stock_id),
+            'CaptchaControl1' : '%s' % prediction,
+            'btnOK' : '查詢'
+            }
+        resp = self.rs.post(self.base_url + self.base_page, data = payload)
+        status_code = BeautifulSoup(resp.text, 'html.parser')
+        try:
+            err = status_code.find('span', {'id':'Label_ErrorMsg'}).find('font').text
+            if err.encode('utf-8') == "查無資料":
+                print 'stock id %s invalid' % stock_id
+                return None
+            elif err.encode('utf-8') == "驗證碼錯誤!":
+                print 'captcha verification failed, requeued stock %s' % stock_id
+                self.stock_ids.append(stock_id)
+                return None
+            elif err != "":
+                print u'unknown error %s' % err
+                return None
+        except AttributeError:
+            self.stock_ids.append(stock_id)
+            return None
+        resp = self.rs.get(self.base_url + self.csv_page)
+        buf = io.StringIO(resp.text)
+        for i in range(2):
+            buf.readline()
+        try:
+            df = pd.read_csv(buf, encoding = 'utf8')
+        except pd.errors.ParserError:
+            print "csv error: requeued id %s" % stock_id
+            self.stock_ids.append(stock_id)
+            return None
+        req = self.rs.get(self.base_url + self.csv_page + "?v=t")
+        dom = BeautifulSoup(req.text, 'html.parser')
+        date_ele = dom.find('td', {'id':'receive_date'})
+        if date_ele is not None:
+            self.date = dateutil.parser.parse(date_ele.text)
+        return df
+
+    def stock_enumeration(self):
+        bar = progressbar.ProgressBar(max_value=len(self.stock_ids))
+        i = 0
+        while len(self.stock_ids) > 0:
+            stock_id = self.stock_ids.pop(0)
+            try:
+                self.captcha = Image.open(io.BytesIO(self.get_captcha(self.get_captcha_src())))
+                processed_captcha = self.ocr.preprocess(self.captcha)
+                prediction = self.ocr.captcha_predict(processed_captcha)
+                df = self.payload_once(prediction, stock_id)
+                if df is not None:
+                    df = self.reshape_df(df)
+                    self.data[stock_id] = df
+                    i += 1
+                    bar.update(i)
+                self.rs = requests.session()
+            except IOError:
+                print "captcha error: requeued id %s" % stock_id
+                self.stock_ids.append(stock_id)
+        return self.combine_data(self.data)
+
+    def reshape_df(self, df):
+        df.columns = ['index', 'broker_code', 'price', 'buy', 'sell', 'Unnamed', 'index.1', 'broker_code.1', 'price.1', 'buy.1', 'sell.1']
+        firsthalf = df[['index', 'broker_code', 'price', 'buy', 'sell']]
+        secondhalf = df[['index.1', 'broker_code.1', 'price.1', 'buy.1', 'sell.1']]
+        secondhalf.columns = ['index', 'broker_code', 'price', 'buy', 'sell']
+        return firsthalf.append(secondhalf)
+
+    def combine_data(self, data):
+        master_df = pd.DataFrame()
+        for stock_id in data:
+            df = data[stock_id]
+            df['stock_id'] = stock_id
+            master_df = master_df.append(df)
+        master_df['datadate'] = self.date
+        master_df = master_df.drop('index', 1)
+        master_df = master_df[pd.notnull(master_df['broker_code'])]
+        return master_df
---
+++ akashperskiy/twse_transactions/transactions_job.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import dateutil
+from sqlalchemy import create_engine
+
+TRANSACTIONS_URL = "http://dataeshop.twse.com.tw/frontend/en/product/downloadFile.jsp?ptid=A06&samplefile=ex-Excel-E.xls"
+
+def generate_ten_minute_buckets():
+    df = pd.read_excel(TRANSACTIONS_URL)
+    df.columns = df.iloc[[0]].as_matrix()[0]
+    df = df.iloc[1:]
+    sums = df[df.columns[-7:]].astype(float).groupby(df.index//62).agg('sum')
+    l = map(lambda x: x * 60, range(1, 28))
+    columns = list(df.columns[-9:-7])
+    columns.append(df.columns[1])
+    avgs = df[columns].astype(float).groupby(df.index//62).mean()
+    l = map(lambda x: x * 60, range(1, 28))
+    avgs['Date'] = df.iloc[l][df.columns[0]].reset_index()[df.columns[0]].apply(dateutil.parser.parse)
+    total = pd.concat([avgs, sums], axis=1)
+    total.columns = ["raising", "decline", "taiex", "datadate", "order_bid", "bid_shares", "order_ask", "ask_shares", "transaction", "transaction_shares", "amount"]
+    return total
+
+if __name__ == "__main__":
+    df = generate_ten_minute_buckets()
+    engine = create_engine("mysql://nipun:Nipun123@130.211.136.197:3306/artur?charset=utf8")
+    df.to_sql('twse_transactions', engine, if_exists="append", index=False)
